9f544f1668b38c8f166f563f0b07aebb7073df0f
lyulito <dimitrov.yulian@gmail.com>
2017-01-19 13:39:28 +0200

    getting some simplecode
    Some simple code i did for the tech modul exam

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = Convert.ToInt32(Console.ReadLine());
+            var runners = Convert.ToInt32(Console.ReadLine());
+            var laps = Convert.ToInt32(Console.ReadLine());
+            var lenght = Convert.ToInt32(Console.ReadLine());
+            var capacity = Convert.ToInt32(Console.ReadLine());
+            var donations = Convert.ToDouble(Console.ReadLine());
+
+            long maxRunners = 0;
+
+            if (capacity * days >= runners)
+            {
+                maxRunners = runners;
+                long totalKm = ((long)maxRunners * laps * lenght) / 1000;
+                Calculate(donations, totalKm);
+            }
+            else
+            {
+                maxRunners = capacity * days;
+                long totalKm = ((long)maxRunners * laps * lenght) / 1000;
+                Calculate(donations, totalKm);
+            }
+        }
+        private static void Calculate(double donations, long totalKm)
+        {
+            double moneyGained = donations * totalKm;
+            Console.WriteLine("Money raised: {0:F2}", moneyGained);
+        }
+    }
+}
